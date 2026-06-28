00e12a9046f436c927aeaafbe7f5d75839f382f9
Vicente Ruiz Escobar <greatdazz3@gmail.com>
2026-06-28 19:06:29 -0400

    correccion host

--- backend/src/mongo/mongo.module.ts
+++ backend/src/mongo/mongo.module.ts
@@ -4,7 +4,7 @@ import { Sensor, SensorSchema } from './schemas/sensor.schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://admin:secreto_iot@localhost:27017/iot_db?authSource=admin'),
+    MongooseModule.forRoot('mongodb://admin:secreto_iot@localhost:27018/iot_db?authSource=admin'),
     MongooseModule.forFeature([{ name: Sensor.name, schema: SensorSchema }]),
   ],
   exports: [

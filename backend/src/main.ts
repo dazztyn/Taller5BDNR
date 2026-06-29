3e53371be471b03a9a1e1f697d2d4c42d0d806dc
Vicente Ruiz Escobar <greatdazz3@gmail.com>
2026-06-29 18:33:43 -0400

    correccion cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
   });
 
   await app.listen(process.env.PORT || 3000);
